691d2db68f6ae5342daa261e387ed0f6a612adf4
Caio-Mizohata <caiomizohata@gmail.com>
2026-05-25 15:19:45 -0300

    feat: refactor app structure and add cursos component
    - Replace placeholder content in app.html with a Material toolbar.
    - Introduce app.scss for theming using Angular Material.
    - Update app.ts to import MatToolbarModule instead of RouterOutlet.
    - Set rootDir in tsconfig.app.json and tsconfig.spec.json for better structure.
    - Create cursos component with its HTML, SCSS, routing, and spec files.

--- crud-app/src/app/app.ts
+++ crud-app/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatToolbarModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss'],
 })
 export class App {
   protected readonly title = signal('crud-app');
