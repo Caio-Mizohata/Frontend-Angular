8ea992cc8b723e7fd55e039778136a74331d1219
Caio-Mizohata <caiomizohata@gmail.com>
2026-06-03 18:26:37 -0300

    feat: adiciona rotas para obter, atualizar e excluir cursos no CursoController

--- backend/src/routes/curso.routes.ts
+++ backend/src/routes/curso.routes.ts
@@ -4,6 +4,9 @@ import CursoController from '../controllers/curso.controller';
 const cursoRoutes: Router = Router();
 
 cursoRoutes.get('/cursos', CursoController.getAllCursos);
+cursoRoutes.get('/cursos/:id', CursoController.getCursoById);
 cursoRoutes.post('/cursos', CursoController.createCurso);
+cursoRoutes.put('/cursos/:id', CursoController.updateCurso);
+cursoRoutes.delete('/cursos/:id', CursoController.deleteCurso);
 
 export default cursoRoutes;
