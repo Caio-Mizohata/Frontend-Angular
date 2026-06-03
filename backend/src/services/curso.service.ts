6b6e6aed6e34a64bac0c3c17fcbe593d943a8bd8
Caio-Mizohata <caiomizohata@gmail.com>
2026-06-03 18:26:41 -0300

    feat: adiciona métodos para obter, atualizar e excluir cursos no CursoService

--- backend/src/services/curso.service.ts
+++ backend/src/services/curso.service.ts
@@ -21,6 +21,17 @@ class CursoService {
         return cursos;
     }
 
+    public async getCursoById(id: number): Promise<ICurso | null> {
+        const query: string = 'SELECT id, nome, categoria FROM cursos WHERE id = ?';
+        const db = await this.db;
+        const raw = db.prepare(query).get(id);
+        if (!raw) {
+            return null;
+        }
+        const curso = CursoSchema.parse(raw);
+        return curso;
+    }
+
     public async createCurso(curso: ICurso): Promise<void> {
         const query: string = 'INSERT INTO cursos (nome, categoria) VALUES (?, ?)';
         const db = await this.db;
@@ -29,6 +40,26 @@ class CursoService {
             throw new Error('Falha ao executar a query de inserção: ' + query);
         }
         create.run(curso.nome, curso.categoria);
+    }
+
+    public async updateCurso(id: number, curso: ICurso): Promise<void> {
+        const query: string = 'UPDATE cursos SET nome = ?, categoria = ? WHERE id = ?';
+        const db = await this.db;
+        const update = db.prepare(query);
+        if (!update) {
+            throw new Error('Falha ao executar a query de atualização: ' + query);
+        }
+        update.run(curso.nome, curso.categoria, id);
+    }
+
+    public async deleteCurso(id: number): Promise<void> {
+        const query: string = 'DELETE FROM cursos WHERE id = ?';
+        const db = await this.db;
+        const del = db.prepare(query);
+        if (!del) {
+            throw new Error('Falha ao executar a query de exclusão: ' + query);
+        }
+        del.run(id);
     }
 }
 
