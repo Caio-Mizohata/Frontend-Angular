9e7b820729227b4c48cfc57fc0b360a71e392126
Caio-Mizohata <caiomizohata@gmail.com>
2026-06-09 18:23:17 -0300

    feat: implementa formulário reativo para cadastro de cursos

--- crud-app/src/app/components/curso-forms/curso-forms.ts
+++ crud-app/src/app/components/curso-forms/curso-forms.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { signal } from '@angular/core';
+import { Curso } from '../../models/curso';
+import { form, required } from '@angular/forms/signals';
+import { MatFormFieldModule}  from '@angular/material/form-field';
 
 @Component({
   selector: 'app-curso-forms',
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule],
   templateUrl: './curso-forms.html',
   styleUrl: './curso-forms.scss',
 })
-export class CursoForms {}
+export class CursoForms {
+
+  protected readonly curso = signal<Curso>({
+    nome: '',
+    categoria:'',
+  })
+
+  cursoForm = form(this.curso, (schemaPath) => {
+    required(schemaPath.nome, {message: 'O nome é obrigatório'});
+    required(schemaPath.categoria, {message: 'A categoria é obrigatória'});
+  });
+
+}
