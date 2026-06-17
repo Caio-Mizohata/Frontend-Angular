2d04811901d280d9de129545f583af9c7a344971
Caio-Mizohata <caiomizohata@gmail.com>
2026-06-16 23:53:03 -0300

    feat: adiciona tratamento de sucesso ao excluir curso com notificação

--- crud-app/src/app/components/cursos/cursos.ts
+++ crud-app/src/app/components/cursos/cursos.ts
@@ -8,6 +8,7 @@ import { catchError, shareReplay } from 'rxjs/operators';
 
 import { AppMaterialModule } from '../../shared/app-material/app-material-module';
 import { ErrorDialog } from '../../shared/components/error-dialog/error-dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Curso } from '../../models/curso';
 import { CursoService } from '../../services/curso-service';
 import { Router } from '@angular/router';
@@ -25,6 +26,7 @@ export class Cursos {
   private cursoService = inject(CursoService);
   private dialog = inject(MatDialog);
   private router = inject(Router);
+  private _snackBar = inject(MatSnackBar);
 
   cursos$: Observable<Curso[]>;
   readonly colunas: string[] = ['nome', 'categoria', 'actions'];
@@ -39,12 +41,40 @@ export class Cursos {
     );
   }
 
-  onError(errorMsg: string): void {
+  private refresh(): void {
+    this.cursos$ = this.cursoService.listar().pipe(
+      catchError(() => {
+        this.onError('Erro ao carregar cursos. Se o problema persistir, contate o suporte.');
+        return of([]); // Retorna um array vazio em caso de erro
+      }),
+    );
+  }
+
+    onDelete(curso: Curso): void {
+    this.cursoService.delete(curso.id!).subscribe({
+      next: () => {
+        this.refresh();
+        this._snackBar.open('Curso excluído com sucesso.', 'Fechar', {
+          duration: 5000,
+          verticalPosition: 'top',
+          horizontalPosition: 'center',
+        });
+      },
+      error: this.onError.bind(this, 'Erro ao excluir curso. Se o problema persistir, contate o suporte.'),
+    });
+  }
+
+  private onError(errorMsg: string): void {
     this.dialog.open(ErrorDialog, {
       data: errorMsg,
     });
   }
+
   onAdd(): void {
     this.router.navigateByUrl('/new');
+  }
+
+  onEdit(curso: Curso): void {
+    this.router.navigateByUrl(`/edit/${curso.id}`);
   }
 }
