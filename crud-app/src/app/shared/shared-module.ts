72bd54f51ae5a2c74666ef1423601c890405bf97
Caio-Mizohata <caiomizohata@gmail.com>
2026-06-23 23:50:08 -0300

    feat: adiciona diálogo de confirmação ao módulo compartilhado

--- crud-app/src/app/shared/shared-module.ts
+++ crud-app/src/app/shared/shared-module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AppMaterialModule } from './app-material/app-material-module';
 import { ErrorDialog } from './components/error-dialog/error-dialog';
 import { CategoryPipe } from './pipes/category-pipe';
+import { ConfirmationDialogue } from '../components/confirmation-dialogue/confirmation-dialogue';
 
 @NgModule({
-  imports: [CommonModule, AppMaterialModule, ErrorDialog, CategoryPipe],
-  exports: [AppMaterialModule, ErrorDialog, CategoryPipe],
+  imports: [CommonModule, AppMaterialModule, ErrorDialog, CategoryPipe, ConfirmationDialogue],
+  exports: [AppMaterialModule, ErrorDialog, CategoryPipe, ConfirmationDialogue],
 })
 export class SharedModule {}
